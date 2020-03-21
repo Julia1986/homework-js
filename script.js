2e88b124b9f84e68a11262937c04b3934ecaf0f4
Juliya <prakhova_yuliya@ukr.net>
2020-03-21 22:37:53 +0200

    second comment

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 let money = prompt("Ваш бюджет на месяц?", ""),
-    time = prompt("Введите дату в формате YYYY-MM-DD", "");
-console.log(money, time);
+    time = prompt("Введите дату в формате YYYY-MM-DD", ""),
+    costs = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    spending = prompt("Введите обязательную статью расходов в этом месяце"),
+    totalCost = prompt("Во сколько обойдется?"),
+
+    appData = {
+        butget: money,
+        timeData: time,
+    }
+        expenses = {
+            name: "spending" + "totalCost",
+        }
+    optionalExpenses = {
+
+    }
+    income = [];
+    savings = false;
